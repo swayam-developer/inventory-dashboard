29eab9c4af0246738ca4120cf52484303bc6a820
swayam sathawane <swayamsathawane@gmail.com>
2025-06-02 13:00:29 +0530

    Update index.tsx

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -67,11 +67,11 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
-              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
+              src="https://s3-inventory-management-bucket1.s3.ap-south-1.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
-              className="rounded-full h-full object-cover"
+              className="rounded-full h-gfull object-cover"
             />
             <span className="font-semibold">Ed Roh</span>
           </div>
